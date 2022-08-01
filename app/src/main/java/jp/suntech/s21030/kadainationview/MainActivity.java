02c7e04b90068a0628b6aa1706ff50f79a1242a7
stc-s21030 <s21030@suntech.jp>
2022-08-01 09:54:56 +0900

    Initial commit

---
+++ app/src/main/java/jp/suntech/s21030/kadainationview/MainActivity.java
@@ -0,0 +1,34 @@
+package jp.suntech.s21030.kadainationview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lvMenu=findViewById(R.id.lvMenu);
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //String item = (String)parent.getItemAtPosition(position);
+            TextView output = findViewById(R.id.Nation);
+            TextView tvText=(TextView)view;
+            String item=tvText.getText().toString();
+            output.setText(item);
+        }
+    }
+}
